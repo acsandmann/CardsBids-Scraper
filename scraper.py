7ac1b0de1e49831d79c49301745c6554cec11ddf
acsandmann <asandmann@icloud.com>
2024-04-23 20:01:50 -0400

    initial code for scraper

--- scraper.py
+++ scraper.py
@@ -6,10 +6,12 @@ import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from concurrent.futures import ThreadPoolExecutor
 import os
 import urllib.request
 import pandas as pd
 import ssl
+import json
 
 ssl._create_default_https_context = ssl._create_stdlib_context
 
@@ -19,7 +21,7 @@ class Scraper:
         options = Options()
         options.add_argument("--window-size=1920,1200")
         options.add_argument('--disable-blink-features=AutomationControlled')
-        # options.add_argument('--headless')
+        #options.add_argument('--headless')
         self.driver = webdriver.Chrome(options=options)
 
         opener=urllib.request.build_opener()
@@ -42,6 +44,35 @@ class Scraper:
             links = [auction.absolute_links for auction in auctions]
             return links
 
+    def scrape_page(self, page=None):
+        soup = self.make_request(f'https://carsandbids.com/past-auctions/{f"?page={page}" if page is not None else ""}')
+        #print(soup.prettify())
+        ul_tag = soup.find("ul", class_ = "auctions-list past-auctions")
+        li_tags = ul_tag.find_all("li", class_ ="auction-item")
+
+
+        for li in li_tags:
+            data = {
+                'url': li.find('a', class_='hero')['href'],
+                'title': li.find('a', class_='hero')['title'],
+                'image_url': li.find('img')['src'],
+                #'sold_price': li.find('span', class_='bid-value').text
+            }
+            bid_value = li.find('span', class_='bid-value')
+            if bid_value:
+                data['sold_price'] = bid_value.text.strip()
+
+            auction_subtitle = li.find('p', class_='auction-subtitle')
+            if auction_subtitle:
+                details = auction_subtitle.text.strip() 
+                data['transmission'] = "manual" if "manual" in details.lower() else "automatic" if "automatic" in details.lower() else "Unknown"
+                data['mileage'] = re.search(r'\b\d{1,3}(,\d{3})*\b miles', details, re.I)
+                data['mileage'] = data['mileage'].group(0) if data['mileage'] else "Unknown"
+                data['ownership'] = "Southern-Owned" if "southern-owned" in details.lower() else "Unknown"
+                data['modifications'] = "modified" if "modifications" in details.lower() or "modified" in details.lower() else "Unmodified"
+
+            self.data.append(data)
+
     def make_request(self, url):
         driver = self.driver
         try:
@@ -61,37 +92,18 @@ class Scraper:
             return soup
         except Exception as e:
             print(e)
-    
-    def get(self, li):
-        auction_url = li.find('a', class_='hero')['href']
-        auction_title = li.find('a', class_='hero')['title']
-        image_url = li.find('img')['src']
-        # sold_price = li.find('span', class_='bid-value').text
-        # description = li.find('p', class_='auction-subtitle').text.strip()
-        # auction_end_date = li.find('p', class_='auction-loc').text.strip()
-        """print("Auction URL:", auction_url)
-        print("Title:", auction_title)
-        print("Image URL:", image_url)
-        print("Sold Price:", sold_price)
-        print("Description:", description)
-        print("Auction End Date:", auction_end_date)"""
-        return { 'url': auction_url,  'title': auction_title }  
 
     def scrape(self):
-        soup = self.make_request('https://carsandbids.com/past-auctions/?page=0')
-        #print(soup.prettify())
-        ul_tag = soup.find("ul", class_ = "auctions-list past-auctions")
-        li_tags = ul_tag.find_all("li", class_ ="auction-item")
-
-
-        for tag in li_tags:
-            #print(tag)
-            self.data.append(self.get(tag))
-
+        pages = range(1, 50)  # As an example, if there are 395 pages
+        with ThreadPoolExecutor(max_workers=5) as executor:
+            executor.map(self.scrape_page, pages)
 def main():
     s = Scraper()
     s.scrape()
     print(s.data)
+    with open('scraped_data.json', 'w') as outfile:
+        json.dump(s.data, outfile, indent=4)
+
 
 if __name__ == '__main__':
     main()
